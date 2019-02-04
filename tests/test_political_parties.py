65d81587f304cf47c02622573c011e9ca76e3b3f
nduhiu17 <nduhiu254@gmail.com>
2019-02-04 21:43:22 +0300

    [Starts #163683903] Create test to post a political party that fails

--- tests/test_political_parties.py
+++ tests/test_political_parties.py
@@ -1,10 +1,21 @@
+import json
+
 from tests.base_test_case import BaseTestCase
+from utils.helpers import party_to_post
 
 
 class TestParties(BaseTestCase):
     """Test case class for political parties"""
+
     def test_get_parties(self):
         """ Test get all political parties"""
         with self.client:
             response = self.client.get('/api/v1/parties')
             self.assertEqual(response._status_code, 200)
+
+    def test_create_party(self):
+        """Test to create a political party"""
+        with self.client:
+            response = self.client.post('api/v1/parties', data=json.dumps(party_to_post),
+                                        headers={'Content-Type': 'application' '/json'})
+            self.assertEqual(response._status_code, 201)
---
+++ utils/helpers.py
@@ -0,0 +1,5 @@
+party_to_post = {
+    "name": "democrat",
+    "hqaddress": "New york",
+    "logoUrl": "www.logo.com/logo"
+}
