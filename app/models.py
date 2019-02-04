323999219864e54cc30adfb1a8718c29658d98c1
nduhiu17 <nduhiu254@gmail.com>
2019-02-04 11:56:31 +0300

    [Feature #163683943] Create a party model and a method to get all parties from the mock database

--- app/models.py
+++ app/models.py
@@ -3,3 +3,31 @@ MOCK_DATABASE = {
 }
 
 
+class Party(object):
+    """Class to model a party"""
+
+    def __init__(self, name, hqaddress, logoUrl):
+        """Initializing the party class"""
+        self.id = len(MOCK_DATABASE["parties"]) + 1
+        self.name = name
+        self.hqaddress = hqaddress
+        self.logoUrl = logoUrl
+
+    def json_dumps(self):
+        """method to return a json object from the party details"""
+        party_obj = {
+            "id": str(self.id),
+            "name": self.name,
+            "hqaddress": self.hqaddress,
+            "logoUrl": self.logoUrl
+        }
+        return party_obj
+
+    @classmethod
+    def get_all(cls):
+        """method to get all parties"""
+        all_parties = MOCK_DATABASE['parties']
+        get_all_json = []
+        for item in all_parties:
+            get_all_json.append(item.json_dumps())
+        return get_all_json
