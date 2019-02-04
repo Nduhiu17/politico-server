049b9f1805850fb525749f19872cde6eaf064270
nduhiu17 <nduhiu254@gmail.com>
2019-02-04 12:12:26 +0300

    [Feature #163683943] Create model method to get all parties and create get all parties endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask_cors import CORS
 
+from app.views import version1
 from instance.config import DevelopmentConfig
 
 app = Flask(__name__)
 CORS(app)
 app.config.from_object(DevelopmentConfig)
+app.register_blueprint(version1)
+
+from . import views, models
