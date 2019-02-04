0b3aa95e04c3a4d7f32985950144a59a803697b2
nduhiu17 <nduhiu254@gmail.com>
2019-02-05 01:12:13 +0300

    [Feature #163683903] Write more tests for edge cases

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@
 from flask import make_response, jsonify, Blueprint, request, abort
 
 from app.models import Party
+from utils.input_validators import Validate
 
 version1 = Blueprint('api-v1', __name__, url_prefix='/api/v1')
 
@@ -19,6 +20,7 @@ def get():
 @version1.route('/parties', methods=['POST'])
 def post():
     """End point to post a political party"""
+
     if not request.json or not 'name' in request.json:
         return make_response(jsonify({
             "status": 400,
@@ -35,8 +37,23 @@ def post():
             "status": 400,
             "error": "logoUrl is required"
         }), 400)
+    data = request.get_json(force=True)
+    if type(data['name']) == int or type(data['hqaddress']) == int or type(data['logoUrl']) == int:
+        return make_response(jsonify({
+            "status": 400,
+            "error": "Post data of type strings"
+        }), 400)
 
-    data = request.get_json()
+    if not Validate.validate_name(name=data["name"]):
+        return make_response(jsonify({
+            "status": 400,
+            "error": "Invalid username"
+        }), 400)
+    if not Validate.validate_name_length(name=data["name"]):
+        return make_response(jsonify({
+            "status": 400,
+            "error": "Name should be atleast 5 characters long"
+        }), 400)
 
     name = data["name"]
     hqaddress = data["hqaddress"]
--- tests/test_political_parties.py
+++ tests/test_political_parties.py
@@ -1,7 +1,8 @@
 import json
 
 from tests.base_test_case import BaseTestCase
-from utils.helpers import party_to_post
+from utils.helpers import party_to_post, party1_to_post, party2_to_post, party3_to_post, party4_to_post, party5_to_post, \
+    party6_to_post, party7_to_post
 
 
 class TestParties(BaseTestCase):
@@ -19,3 +20,26 @@ class TestParties(BaseTestCase):
             response = self.client.post('api/v1/parties', data=json.dumps(party_to_post),
                                         headers={'Content-Type': 'application' '/json'})
             self.assertEqual(response._status_code, 201)
+            response = self.client.post('api/v1/parties', data=json.dumps(party1_to_post),
+                                        headers={'Content-Type': 'application' '/json'})
+            self.assertEqual(response._status_code, 400)
+            response = self.client.post('api/v1/parties', data=json.dumps(party2_to_post),
+                                        headers={'Content-Type': 'application' '/json'})
+            self.assertEqual(response._status_code, 400)
+            response = self.client.post('api/v1/parties', data=json.dumps(party3_to_post),
+                                        headers={'Content-Type': 'application' '/json'})
+            self.assertEqual(response._status_code, 400)
+            response = self.client.post('api/v1/parties', data=json.dumps(party4_to_post),
+                                        headers={'Content-Type': 'application' '/json'})
+            self.assertEqual(response._status_code, 400)
+            response = self.client.post('api/v1/parties', data=json.dumps(party5_to_post),
+                                        headers={'Content-Type': 'application' '/json'})
+            self.assertEqual(response._status_code, 400)
+            response = self.client.post('api/v1/parties', data=json.dumps(party6_to_post),
+                                        headers={'Content-Type': 'application' '/json'})
+            self.assertEqual(response._status_code, 400)
+            response = self.client.post('api/v1/parties', data=json.dumps(party7_to_post),
+                                        headers={'Content-Type': 'application' '/json'})
+            self.assertEqual(response._status_code, 400)
+
+
