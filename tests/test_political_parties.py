84109e05298db2855091232beca41f8146739395
nduhiu17 <nduhiu254@gmail.com>
2019-02-05 10:51:23 +0300

    [Starts #163684067] Create test to edit a party that fails

--- tests/test_political_parties.py
+++ tests/test_political_parties.py
@@ -2,7 +2,7 @@ import json
 
 from tests.base_test_case import BaseTestCase
 from utils.helpers import party_to_post, party1_to_post, party2_to_post, party3_to_post, party4_to_post, party5_to_post, \
-    party6_to_post, party7_to_post
+    party6_to_post, party7_to_post, party8_to_post
 
 
 class TestParties(BaseTestCase):
@@ -56,4 +56,13 @@ class TestParties(BaseTestCase):
             response1 = self.client.get('api/v1/parties/10000', headers={'Content-Type': 'application' '/json'})
             self.assertEqual(response1._status_code, 404)
 
+    def test_edit_party(self):
+        """Tests for editing a political party"""
+        with self.client:
+            response = self.client.post('api/v1/parties', data=json.dumps(party_to_post),
+                                        headers={'Content-Type': 'application' '/json'})
+            self.assertEqual(response._status_code, 201)
+            response = self.client.put('api/v1/parties/1', data=json.dumps(party8_to_post),
+                                        headers={'Content-Type': 'application' '/json'})
+            self.assertEqual(response._status_code, 201)
 
