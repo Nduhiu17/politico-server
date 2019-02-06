3582dc4b9914a013b0465d3ff0d30b79fd204e0b
nduhiu17 <nduhiu254@gmail.com>
2019-02-06 10:46:46 +0300

    [Feature #163684147] Create endpoint to post an office

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,13 @@
 from flask import Flask
 from flask_cors import CORS
 
-from app.views import version1
+from app.views import version1, V1
 from config import DevelopmentConfig
 
 app = Flask(__name__)
 CORS(app)
 app.config.from_object(DevelopmentConfig)
 app.register_blueprint(version1)
+app.register_blueprint(V1)
 
 from . import views, models
