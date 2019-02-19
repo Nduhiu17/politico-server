5e2aee7ebeb008ed8298297c10e347c3e5e5a019
nduhiu17 <nduhiu254@gmail.com>
2019-02-19 11:22:08 +0300

    feature(get a party)user can get a specific party -Create tests that a party can be retrieved

--- example_start.sh
+++ example_start.sh
@@ -1,5 +1,6 @@
 source virtual/bin/activate
 export SECRET_KEY='your-secret-key-comes-here'
-export DATABASE_URL="postgres://yourusername:yourpassword@localhost:5432/your-test-db-name"
+export JWT_SECRET_KEY='your-secret-key-comes-here'
+export DATABASE_URL="postgres://yourusername:yourpassword@localhost:5432/your-db-name"
 
 python manage.py server
